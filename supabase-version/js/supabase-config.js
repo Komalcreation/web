607caa093d8f92860b3107345d22c4b0ef4de7da
Komalcreation <komalcreationnabha@gmail.com>
2026-06-21 11:57:49 +0530

    Update supabase-config.js

--- supabase-version/js/supabase-config.js
+++ supabase-version/js/supabase-config.js
@@ -7,7 +7,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 // IMPORTANT: Replace these strings with your Supabase credentials
-const SUPABASE_URL = 'https://your-project-id.supabase.co';
-const SUPABASE_ANON_KEY = 'your-anon-role-public-api-key-goes-here';
+const SUPABASE_URL = 'kmxnjnynjqtzjbtiuhdx';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtteG5qbnluanF0empidGl1aGR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMDk1MzEsImV4cCI6MjA5NzU4NTUzMX0.9UEUP0RIydInJeDLqlIMZYGfSuJlW_8pdZ9tUIzKcMg';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
